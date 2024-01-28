097cc9781045d70529ed389c788f70021e5d996a
HomeLucker <157958752+HomeLucker@users.noreply.github.com>
2024-01-28 14:10:38 +0300

    Add files via upload

---
+++ final_projekt/E-Szpital/AddBuilding.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Szpital
+{
+    internal class AddBuilding : Building
+    {
+        public void DodacBudynek(string filePath)
+        {
+            Console.WriteLine("Wpisz Nazwę: ");
+            string nazwa = Console.ReadLine();
+            Console.WriteLine("Wpisz Adres: ");
+            string adres = Console.ReadLine();
+            Console.WriteLine("Wpisz Dzielnicę: ");
+            string dzielnica = Console.ReadLine();
+            Building budynek = new Building { Nazwa = nazwa, Adres = adres, Dzielnica = dzielnica };
+
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = !File.Exists(filePath),
+            };
+
+            using (var writer = new StreamWriter(filePath, append: true))
+            using (var csv = new CsvWriter(writer, config))
+            {
+                csv.WriteRecord(budynek);
+            }
+
+            Console.WriteLine("Dane dodane do pliku CSV.");
+        }
+    }
+}
---
+++ final_projekt/E-Szpital/SignIn.cs
@@ -0,0 +1,36 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Szpital
+{
+    internal class SignIn : Program
+    {
+
+        public string Login { get; set; }
+        public string Haslo { get; set; }
+
+
+        public bool Logowanie(string filePath, string login, string haslo)
+        {
+            using (var reader = new StreamReader(filePath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<SignIn>().ToList();
+
+                foreach (var uzytkownik in records)
+                {
+                    if (uzytkownik.Login == login && uzytkownik.Haslo == haslo)
+                    {
+                        return true; 
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
